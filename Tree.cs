19ef02b3dc02549e2dae43ae062c859e1958318e
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-11 21:32:29 +0200

    Updated Tree.cs method.

--- Tree.cs
+++ Tree.cs
@@ -1,6 +1,20 @@
 ﻿namespace DataStructures.Collections.Trees;
 
-public class Tree
+public class Tree<T>
 {
+    public TreeNode<T> Root { get; set; }
 
+    public Tree(T data)
+    {
+        this.Root = new TreeNode<T>(data);
+    }
+
+    public void Traverse(TreeNode<T> node, Action<T> action)
+    {
+        action(node.Data);
+        foreach (TreeNode<T> child in node.Children)
+        {
+            Traverse(child, action);
+        }
+    }
 }
