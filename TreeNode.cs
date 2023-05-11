bd4490f78953ea899c60bc0dabdd7d51cda4137a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-11 21:22:38 +0200

    Added private member.

--- TreeNode.cs
+++ TreeNode.cs
@@ -3,6 +3,7 @@ namespace DataStructures.Collections.Trees;
 public class TreeNode<T>
 {
     private T _data;
+    private TreeNode<T> _parent;
 
     public T Data { get; set; }
     public TreeNode<T> Parent { get; set; }
