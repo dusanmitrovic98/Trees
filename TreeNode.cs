f594fc539bb3003f30d7dc2b1d990638f6c80783
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-11 21:23:50 +0200

    Updated Data property.

--- TreeNode.cs
+++ TreeNode.cs
@@ -6,7 +6,11 @@ public class TreeNode<T>
     private TreeNode<T> _parent;
     private List<TreeNode<T>> _children;
 
-    public T Data { get; set; }
+    public T Data
+    {
+        get { return this._data; }
+        set { this._data = value; }
+    }
     public TreeNode<T> Parent { get; set; }
     public List<TreeNode<T>> Children { get; set; }
 }
