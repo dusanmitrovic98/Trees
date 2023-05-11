c3cd405279ff749eed71359651c87cd3c66b586b
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-11 21:23:11 +0200

    Added private member.

--- TreeNode.cs
+++ TreeNode.cs
@@ -4,6 +4,7 @@ public class TreeNode<T>
 {
     private T _data;
     private TreeNode<T> _parent;
+    private List<TreeNode<T>> _children;
 
     public T Data { get; set; }
     public TreeNode<T> Parent { get; set; }
