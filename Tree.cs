c3748b0dc614d8526845e8bdd9026602cea910f4
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-12 20:08:58 +0200

    Updated Tree.cs method.

--- Tree.cs
+++ Tree.cs
@@ -2,19 +2,5 @@
 
 public class Tree<T>
 {
-    public TreeNode<T> Root { get; set; }
 
-    public Tree(T data)
-    {
-        this.Root = new TreeNode<T>(data);
-    }
-
-    public void Traverse(TreeNode<T> node, Action<T> action)
-    {
-        action(node.Data);
-        foreach (TreeNode<T> child in node.Children)
-        {
-            Traverse(child, action);
-        }
-    }
 }
